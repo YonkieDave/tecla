df971727c82a45a95c220206929e738f7c680291
YonkieDave <dtames1714@gmail.com>
2021-08-19 19:52:13 -0500

    Creacion de funciones asincronas

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function saludo() {
+/*function saludo() {
     console.log("Hola ya estoy en git");
 }
+
+function llamada() {
+    console.log("Hola mundo");
+}
+
+function llamadora(call) {
+    console.log("dentro de esta función llamamos nuestro callback");
+    call()
+}
+
+llamadora(llamada);*/
+
+console.log("Esto va primero");
+setTimeout(() => {
+    console.log("Esto va segundo");
+}, 2000);
+console.log("Esto va tercero");
